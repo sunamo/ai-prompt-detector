d5da93913d1b3fdf690a5510d9e1f80c58ce7ad4
Radek Jancik <radek.jancik@usys.cz>
2025-08-16 20:26:20 +0200

    v1.1.428
    ...

--- install.ps1
+++ install.ps1
@@ -232,7 +232,7 @@ $1
     
     # Spusť Code - OSS s projektem
     Write-Host "   - Starting Code - OSS..." -ForegroundColor Gray
-    $codeOssPath = 'E:\vs\TypeScript_Projects\_\vscode\.build\electron\Code - OSS.exe'
+    $codeOssPath = 'E:\vs\TypeScript_Projects\_\VSCode-win32-x64\Code - OSS.exe'
     Start-Process -FilePath $codeOssPath -ArgumentList 'E:\vs\TypeScript_Projects\_\vscode' -WindowStyle Normal
     
     Write-Host "   ✅ Code - OSS restarted" -ForegroundColor Green
